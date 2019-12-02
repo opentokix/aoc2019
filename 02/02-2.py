b7ad7384eb918048f0b0acc7f9f5b330c996f0cb
OpenTokix <petereriksson.net@gmail.com>
2019-12-02 22:01:53 +0100

    day 2 done

--- 02/02-2.py
+++ 02/02-2.py
@@ -3,12 +3,13 @@ from sys import exit
 
 def get_input():
     f = open("input","r")
-    content = f.read()
-    content = content.rstrip()
-    l = content.split(',')
-    wip = list(map(int, l))
-    return wip
+    return listify(f.read())
 
+
+def listify(s):
+    r = s.rstrip()
+    l = r.split(',')    
+    return list(map(int, l)) 
 
 def calc(l, pos):
     if l[pos] == 2:
@@ -23,38 +24,60 @@ def calc(l, pos):
        val2 = l[l[pos+2]]
        l[store_pos] = val1 + val2 
        return l, l[pos]
-    elif l[pos] == 99:
-        return l, l[pos]
-    else: 
-        print("unknown op code", l[pos])
-        return l, l[pos]
 
 def run_program(l):
     for i in range(0, len(l), 4):
         wip, op = calc(wip, i)
-    if op == '99':
-        return wip[0]
-    else: 
-        print ("error: ", wip[0])
-        return 1
+
+def test_list(l, noun=None, verb=None):
+    if noun != None:
+        l[1] = noun
+    if verb != None:
+        l[2] = verb
+    for i in range(0, len(l), 4):
+        if l[i] == 99:
+            break
+        if l[i] not in (1, 2):
+            break
+        l, _ = calc(l, i)
+    return l
+
+def t1(s1, s2):
+    s1 = listify(s1)
+    r = test_list(s1)
+    final = ','.join(map(str, r))
+    if final == s2:
+        print('true')
+    else:
+        print('false', r, "res: ", s2)
 
 def main():
-    initial = get_input()
-    num_runs = 0 
-    for noun in range(0, 99):
-        for verb in range(0, 99):
-            num_runs += 1 
-            wip = initial  
-            wip[1] = noun
-            wip[2] = verb
-            print(num_runs)
-            for i in range(0, len(wip), 4):
-                wip = calc(wip, i)
-                if wip[0] == "19690720":
-                    result = 100 * noun + verb 
-                    print(result)
-                    exit(0)
-    
+    ex1 = "1,0,0,0,99"
+    ex2 = "2,3,0,3,99"
+    ex3 = "2,4,4,5,99,0"
+    ex4 = "1,1,1,4,99,5,6,0,99"
+
+    res1 = "2,0,0,0,99"
+    res2 = "2,3,0,6,99"
+    res3 = "2,4,4,5,99,9801"
+    res4 = "30,1,1,4,2,5,6,0,99"
+
+    for test in ['ex1', 'ex2', 'ex3', 'ex4']:
+        if test == 'ex1':
+            t1(ex1, res1)
+        elif test == 'ex2':
+            t1(ex2, res2)
+        elif test == 'ex3':
+            t1(ex3, res3)
+        elif test == 'ex4':        
+            t1(ex4, res4)
+    for i in range(0, 99):
+        for j in range(0, 99):
+            start_list = get_input()
+            foo = test_list(start_list, i, j)
+            if foo[0] == 19690720:
+                r = 100 * i + j
+                print(r)
 
 if __name__ == '__main__':
     main()
