df240819597b36fd414c8ca148688f73710fa500
OpenTokix <petereriksson.net@gmail.com>
2019-12-03 19:12:33 +0100

    wip, so close

--- 03/03-1.py
+++ 03/03-1.py
@@ -2,7 +2,7 @@
 import numpy as np
 import scipy.misc as smp
 from PIL import Image
-size = 24000
+size = 20
 dists = []
 global white
 white = np.asarray([255, 255, 255], dtype=np.uint8)
@@ -11,7 +11,8 @@ black = np.asarray([0, 0, 0], dtype=np.uint8)
 
 global canvas
 canvas = np.zeros((size+1, size+1, 3), dtype=np.uint8)
-
+grey = np.asarray([70, 70, 70], dtype=np.uint8)
+dgrey = np.asarray([35, 35, 35], dtype=np.uint8)
 
 colors = {1: np.asarray([200,50,50], dtype=np.uint8),
           2: np.asarray([75,75,200], dtype=np.uint8),
@@ -20,7 +21,7 @@ colors = {1: np.asarray([200,50,50], dtype=np.uint8),
           5: np.asarray([10,50,250], dtype=np.uint8),
           6: np.asarray([50,50,50], dtype=np.uint8),
           7: np.asarray([100,50,50], dtype=np.uint8),
-          8: np.asarray([150,50,50], dtype=np.uint8)}
+          8: np.asarray([0,255,0], dtype=np.uint8)}
 
 def calc_manhattan(x, y):
     c_x = size//2
@@ -31,7 +32,7 @@ def insert_pixel(x, y, color):
     canvas[x, y] = color
 
 def check_pixel(x, y, c):
-    if np.array_equal(canvas[x, y], black):
+    if np.array_equal(canvas[x, y], black) or np.array_equal(canvas[x, y], grey) or np.array_equal(canvas[x, y], dgrey):
         return True
     elif np.array_equal(canvas[x, y], c):
         return True
@@ -42,21 +43,21 @@ def check_pixel(x, y, c):
 def up(steps, pos, color):
     i = 0
     for i in range(0, steps+1):
-        if check_pixel(pos[0], pos[1]+i, color):
-            insert_pixel(pos[0], pos[1]+i, color)
-        else:
-            insert_pixel(pos[0], pos[1]+i, white)
-    r = [pos[0], pos[1]+i]
-    return r
-
-def down(steps, pos, color):
-    i = 0
-    for i in range(0, steps+1):
         if check_pixel(pos[0], pos[1]-i, color):
             insert_pixel(pos[0], pos[1]-i, color)
         else:
             insert_pixel(pos[0], pos[1]-i, white)
     r = [pos[0], pos[1]-i]
+    return r
+
+def down(steps, pos, color):
+    i = 0
+    for i in range(0, steps+1):
+        if check_pixel(pos[0], pos[1]+i, color):
+            insert_pixel(pos[0], pos[1]+i, color)
+        else:
+            insert_pixel(pos[0], pos[1]+i, white)
+    r = [pos[0], pos[1]+i]
     return r
 
 def left(steps, pos, color):
@@ -106,6 +107,7 @@ def render_file(in_file, expected=None, img=None):
             line_num += 1
             pos = [size//2, size//2]
             line = f.readline()
+    canvas[size//2, size//2] = colors[8]
     numbers = list(filter(lambda num: num != 0, dists))
     numbers.sort()
     print("file: ", in_file, "result: ", min(numbers))
@@ -116,14 +118,22 @@ def render_file(in_file, expected=None, img=None):
         Image.fromarray(canvas, mode='RGB').save(img)
 
 def main():
-
+    """
+    test_canvas = np.zeros((10, 10, 3), dtype=np.uint8)
+    test_canvas[0, 0] = colors[8]
+    Image.fromarray(test_canvas, mode="RGB").save('test_canv.png')
+    """
+    for x in range(0, size, 2):
+        for y in range(0, size, 2):
+            canvas[x, y] = grey
+            canvas[x+1, y+1] = grey
     render_file("test0", 6, 'test0.png')
-"""    dists.clear()
+    """    dists.clear()
     render_file("test1", 159, 'test1.png')
     dists.clear()
     render_file("test2", 135, 'test2.png')    
     dists.clear()
     render_file("input", 'pic1.png')
-"""
+    """
 if __name__ == '__main__':
     main()
