9c4998d26437da7016f8f6a4965523fd4abac392
OpenTokix <petereriksson.net@gmail.com>
2019-12-03 07:23:33 +0100

    wip

--- 03/03-1.py
+++ 03/03-1.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
+import numpy as np
 
 def main():
     f = open("input","r")
     content = f.read()
+    w, h = np.meshgrid(np.arange(10), np.arange(10), sparse=False)
+    data = []
+    for i in w:
+        for j in h:
+            data[i][j] = '.'
+    print(data)
 
 if __name__ == '__main__':
     main()
